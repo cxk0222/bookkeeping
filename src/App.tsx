8b3b1d66bb3195e5681d248b8feb1577a2826852
cxk0222 <642192014@qq.com>
2024-04-13 20:56:14 +0800

    引入 vue-router 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent } from 'vue';
+import { RouterView } from "vue-router";
 
 export const App = defineComponent({
-  setup() {
-    const count = ref(0)
-    const onClick = () => {
-      count.value += 1
-    }
+  setup: () => {
     return () => <>
-      <div>{ count.value }</div>
+      <header>
+        <ul>
+          <li>
+            <router-link to="/">Foo</router-link>
+          </li>
+          <li>
+            <router-link to="/bar">Bar</router-link>
+          </li>
+        </ul>
+      </header>
       <div>
-        <button onClick={onClick}>+1</button>
+        <RouterView></RouterView>
       </div>
+      <footer>页脚</footer>
     </>
   }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
 import { App } from './App.tsx'
+import { Foo } from './views/Foo.tsx'
+import { Bar } from './views/Bar.tsx'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Foo },
+  { path: '/bar', component: Bar },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
