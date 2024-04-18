2531b92a10694ad07a6c929bc99169d1b3acf132
cxk0222 <642192014@qq.com>
2024-04-19 00:05:43 +0800

    feature: 创建欢迎界面的页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,23 @@
+import { RouteRecordRaw } from "vue-router";
 import { Foo } from "../views/Foo";
 import { Bar } from "../views/Bar";
+import { Welcome } from "../views/Welcome";
+import { First } from "../components/welcome/First";
+import { Second } from "../components/welcome/Second";
+import { Third } from "../components/welcome/Third";
+import { Forth } from "../components/welcome/Forth";
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: '/', component: Foo },
   { path: '/bar', component: Bar },
+  {
+    path: '/welcome',
+    component: Welcome,
+    children: [
+      { path: '1', component: First },
+      { path: '2', component: Second },
+      { path: '3', component: Third },
+      { path: '4', component: Forth },
+    ]
+  },
 ]
